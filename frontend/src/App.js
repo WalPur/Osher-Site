85db4cca46911f74ada7d29219d2d459a4822662
WalPur <mishkamba1@gmail.com>
2022-10-21 12:39:52 +0900

    hello world mobile first app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,14 @@ import {
 } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Mobile from './pages/Mobile';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="" element={<Home/>}></Route>
+          <Route path="" element={<Mobile/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/pages/Mobile.jsx
@@ -0,0 +1,14 @@
+import Components from "../components";
+import React from "react";
+
+function Mobile() {
+    return(
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    )
+}
+
+export default Mobile;
